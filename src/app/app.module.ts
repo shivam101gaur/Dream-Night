aa5aafa3662fbaeca45d59d5dbd652c45018d4c6
shivam101gaur <gaurs3862@gmail.com>
2021-08-28 18:15:35 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateTaskComponent } from './components/create-task/create-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateTaskComponent
   ],
   imports: [
     BrowserModule,

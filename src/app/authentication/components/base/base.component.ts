fa0e3d64f90871e87bb23a92e5bcfa3de71544c7
shivam101gaur <gaurs3862@gmail.com>
2021-08-30 16:54:04 +0530

    cannot

--- src/app/authentication/components/base/base.component.ts
+++ src/app/authentication/components/base/base.component.ts
@@ -9,10 +9,37 @@ export class BaseComponent implements OnInit {
 
   starCountArray = new Array(50)
 
+  // call this to Disable
+  disableScroll() {
+
+    var supportsPassive = false;
+    try {
+      window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; }
+      }));
+
+      var wheelOpt = supportsPassive ? { passive: false } : false;
+      var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+    } catch (e) { }
+
+    window.addEventListener(wheelEvent, (e) => {
+      e.preventDefault();
+    }, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', (e) => {
+      e.preventDefault();
+    }, wheelOpt); // mobile
+    window.addEventListener('keydown', (e) => {
+      if (e.ctrlKey && (e.key == '=' || e.key == '-' || e.key == '_' || e.key == '+')) {
+        e.preventDefault();
+        return false;
+      }
+    }, false);
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    this.disableScroll()
   }
 
 }
